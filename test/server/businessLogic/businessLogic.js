4ee10ff89eefe1b77526ddcb1e4f7c3890848705
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-10-13 17:19:04 -0700

    Added test for mock emailer.

--- test/server/businessLogic/businessLogic.js
+++ test/server/businessLogic/businessLogic.js
@@ -1,26 +1,41 @@
 var assert = require('assert');
 var mockBookshelf = require('./mockBookshelf');
+var mockEmailer = require('./mockEmailer');
 
 describe('business logic', function () {
 
-  it('should have a working mock Bookshelf', function (done) {
-    var Model = mockBookshelf.model('Dummy', ['extraStub']);
-    var model1 = new Model();
-    var model2 = new Model();
+  describe('mock objects', function () {
 
-    assert.strictEqual(Model.callCount, 2, 'The model constructor was not called twice.');
+    it('should have a working mock Bookshelf', function (done) {
+      var Model = mockBookshelf.model('Dummy', ['extraStub']);
+      var model1 = new Model();
+      var model2 = new Model();
 
-    model1.save();
+      assert.strictEqual(Model.callCount, 2, 'The model constructor was not called twice.');
 
-    assert(model1.save.called, 'model1.save() was not called.');
-    assert(!(model2.save.called), 'model2.save() was called.');
+      model1.save();
 
-    model1.extraStub();
+      assert(model1.save.called, 'model1.save() was not called.');
+      assert(!(model2.save.called), 'model2.save() was called.');
 
-    assert(model1.extraStub.called, 'model1.extraStub() was not called.');
-    assert(!(model2.extraStub.called), 'model2.extraStub() was called.');
+      model1.extraStub();
 
-    done();
+      assert(model1.extraStub.called, 'model1.extraStub() was not called.');
+      assert(!(model2.extraStub.called), 'model2.extraStub() was called.');
+
+      done();
+    });
+
+    it('should have a working mock emailer', function (done) {
+      var emailAddress = 'email@address.com';
+      var message = 'This is a message.';
+      mockEmailer(emailAddress, message);
+
+      assert(mockEmailer.withArgs(emailAddress, message).calledOnce, 'The emailer was not called properly.');
+
+      done();
+    });
+
   });
 
   // a list of all of the business logic test modules
