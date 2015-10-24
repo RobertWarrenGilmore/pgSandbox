bfe4038c0140260d5b9883cef0415f8632e58781
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-10-24 07:47:39 -0700

    Tinkered with set-up scripts.

--- server/database/dbSetup.sh
+++ server/database/dbSetup.sh
@@ -1,7 +1,8 @@
 # This script installs Postgres, creates the user for the app, and creates the database.
+echo "Enter a password for the database user:"
+read dbPassword
 
 apt-get --assume-yes install postgresql postgresql-contrib
-password=$(date +%s | sha256sum | base64 | head -c 32)
-sudo -u postgres psql --command="CREATE ROLE \"sandboxUser\" LOGIN PASSWORD '${password}' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
+sudo -u postgres psql --command="CREATE ROLE \"sandboxUser\" LOGIN PASSWORD '${dbPassword}' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 sudo -u postgres psql --command="CREATE DATABASE sandbox WITH ENCODING='UTF8' OWNER=\"sandboxUser\" CONNECTION LIMIT=-1;"
-echo $password > ./database/dbPassword
+echo $dbPassword > ./server/database/dbPassword
