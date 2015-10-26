410b994804669c84e37f98ce57b57ba5efc66328
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-10-26 09:10:29 -0700

    Added error tests.

--- test/server/transport/user.js
+++ test/server/transport/user.js
@@ -9,98 +9,194 @@ var bodyParser = require('body-parser');
 
 describe('user', function () {
 
-  context('as a unit', function () {
-    var emailAddress = 'mocha.test.email.address@not.a.real.domain.com';
-    var password = 'taco tuesday';
+  var emailAddress = 'mocha.test.email.address@not.a.real.domain.com';
+  var password = 'taco tuesday';
 
-    var requestAuth = {
-      emailAddress: emailAddress,
-      password: password
-    };
-    var requestBody;
-    var responseBody;
+  var requestAuth = {
+    emailAddress: emailAddress,
+    password: password
+  };
+  var requestBody;
+  var responseBody;
 
-    var bizQueue = [];
+  var bizQueue = [];
 
-    function bizPromiseReturner() {
-      var result = bizQueue.pop();
-      if (result instanceof Error) {
-        return Promise.reject(result);
-      } else {
-        return Promise.resolve(result);
-      }
+  function bizPromiseReturner() {
+    var result = bizQueue.pop();
+    if (result instanceof Error) {
+      return Promise.reject(result);
+    } else {
+      return Promise.resolve(result);
     }
-    var userBiz = {
-      create: sinon.spy(bizPromiseReturner),
-      read: sinon.spy(bizPromiseReturner),
-      update: sinon.spy(bizPromiseReturner)
+  }
+  var userBiz = {
+    create: sinon.spy(bizPromiseReturner),
+    read: sinon.spy(bizPromiseReturner),
+    update: sinon.spy(bizPromiseReturner)
+  };
+
+  var server;
+
+  before('Host the user transport module on a server.', function () {
+    var userRouter = require('../../../server/transport/user')(userBiz);
+    var app = express();
+    app.use(function authMiddleware(req, res, next) {
+      var auth = parseAuth(req);
+      if (auth) {
+        req.auth = {
+          emailAddress: auth.name,
+          password: auth.pass
+        };
+      }
+      next();
+    });
+    app.use(bodyParser.json({
+      type: 'application/json'
+    }));
+    app.use('/api/users', userRouter);
+    server = http.createServer(app);
+    server.listen(3000);
+  });
+
+  after('Close the server.', function () {
+    server.close();
+  });
+
+  beforeEach('Reset the userBiz stubs.', function () {
+    userBiz.create.reset();
+    userBiz.read.reset();
+    userBiz.update.reset();
+    bizQueue.length = 0;
+
+    requestBody = {
+      foo: 'bar'
     };
+    responseBody = {
+      hello: 'world'
+    };
+    bizQueue.unshift(responseBody);
+  });
 
-    var server;
+  describe('/users', function () {
 
-    before('Host the user transport module on a server.', function () {
-      var userRouter = require('../../../server/transport/user')(userBiz);
-      var app = express();
-      app.use(function authMiddleware(req, res, next) {
-        var auth = parseAuth(req);
-        if (auth) {
-          req.auth = {
-            emailAddress: auth.name,
-            password: auth.pass
-          };
-        }
-        next();
+    it('should create on POST', function () {
+      var requestMatcher = sinon.match({
+        auth: sinon.match(requestAuth),
+        params: sinon.match({}),
+        query: sinon.match({}),
+        body: sinon.match(requestBody)
       });
-      app.use(bodyParser.json({
-        type: 'application/json'
-      }));
-      app.use('/api/users', userRouter);
-      server = http.createServer(app);
-      server.listen(3000);
-    });
-
-    after('Close the server.', function () {
-      server.close();
-    });
-
-    beforeEach('Reset the userBiz stubs.', function () {
-      userBiz.create.reset();
-      userBiz.read.reset();
-      userBiz.update.reset();
-      bizQueue.length = 0;
-
-      requestBody = {
-        foo: 'bar'
-      };
-      responseBody = {
-        hello: 'world'
-      };
-      bizQueue.unshift(responseBody);
-    });
-
-    describe('/users', function () {
-
-      it('should create on POST', function () {
-        var requestMatcher = sinon.match({
-          auth: sinon.match(requestAuth),
-          params: sinon.match({}),
-          query: sinon.match({}),
-          body: sinon.match(requestBody)
-        });
-        return ajax({
-          method: 'POST',
-          uri: 'http://localhost:3000/api/users',
-          auth: {
-            user: emailAddress,
-            pass: password
-          },
-          json: true,
-          body: requestBody
-        }).then(function (response) {
-          assert(userBiz.create.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
-          assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
-        });
+      return ajax({
+        method: 'POST',
+        uri: 'http://localhost:3000/api/users',
+        auth: {
+          user: emailAddress,
+          pass: password
+        },
+        json: true,
+        body: requestBody
+      }).then(function (response) {
+        assert(userBiz.create.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
+        assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
       });
+    });
+
+    it('should read on GET', function () {
+      var requestMatcher = sinon.match({
+        auth: sinon.match(requestAuth),
+        params: sinon.match({}),
+        query: sinon.match({}),
+        body: sinon.match(requestBody)
+      });
+      return ajax({
+        method: 'GET',
+        uri: 'http://localhost:3000/api/users',
+        auth: {
+          user: emailAddress,
+          pass: password
+        },
+        json: true,
+        body: requestBody
+      }).then(function (response) {
+        assert(userBiz.read.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
+        assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
+      });
+    });
+
+    it('should update on PUT', function () {
+      var requestMatcher = sinon.match({
+        auth: sinon.match(requestAuth),
+        params: sinon.match({}),
+        query: sinon.match({}),
+        body: sinon.match(requestBody)
+      });
+      return ajax({
+        method: 'PUT',
+        uri: 'http://localhost:3000/api/users',
+        auth: {
+          user: emailAddress,
+          pass: password
+        },
+        json: true,
+        body: requestBody
+      }).then(function (response) {
+        assert(userBiz.update.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
+        assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
+      });
+    });
+
+    it('should forward defined errors', function () {
+      var requestMatcher = sinon.match({
+        auth: sinon.match(requestAuth),
+        params: sinon.match({}),
+        query: sinon.match({}),
+        body: sinon.match(requestBody)
+      });
+      bizQueue.pop();
+      var error = new Error();
+      error.errorCode = 599;
+      bizQueue.unshift(error);
+      return ajax({
+        method: 'POST',
+        uri: 'http://localhost:3000/api/users',
+        auth: {
+          user: emailAddress,
+          pass: password
+        },
+        json: true,
+        body: requestBody
+      }).then(function (response) {
+        assert(userBiz.create.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
+        assert.equal(response.statusCode, error.errorCode, 'The router returned the wrong thing.');
+      });
+    });
+
+    it('should forward generic errors', function () {
+      var requestMatcher = sinon.match({
+        auth: sinon.match(requestAuth),
+        params: sinon.match({}),
+        query: sinon.match({}),
+        body: sinon.match(requestBody)
+      });
+      bizQueue.pop();
+      var error = new Error();
+      bizQueue.unshift(error);
+      return ajax({
+        method: 'POST',
+        uri: 'http://localhost:3000/api/users',
+        auth: {
+          user: emailAddress,
+          pass: password
+        },
+        json: true,
+        body: requestBody
+      }).then(function (response) {
+        assert(userBiz.create.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
+        assert.equal(response.statusCode, 500, 'The router returned the wrong thing.');
+      });
+    });
+
+    describe('/:userId', function () {
 
       it('should read on GET', function () {
         var requestMatcher = sinon.match({
@@ -111,7 +207,7 @@ describe('user', function () {
         });
         return ajax({
           method: 'GET',
-          uri: 'http://localhost:3000/api/users',
+          uri: 'http://localhost:3000/api/users/123',
           auth: {
             user: emailAddress,
             pass: password
@@ -133,7 +229,7 @@ describe('user', function () {
         });
         return ajax({
           method: 'PUT',
-          uri: 'http://localhost:3000/api/users',
+          uri: 'http://localhost:3000/api/users/123',
           auth: {
             user: emailAddress,
             pass: password
@@ -144,54 +240,6 @@ describe('user', function () {
           assert(userBiz.update.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
           assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
         });
-      });
-
-      describe('/:userId', function () {
-
-        it('should read on GET', function () {
-          var requestMatcher = sinon.match({
-            auth: sinon.match(requestAuth),
-            params: sinon.match({}),
-            query: sinon.match({}),
-            body: sinon.match(requestBody)
-          });
-          return ajax({
-            method: 'GET',
-            uri: 'http://localhost:3000/api/users/123',
-            auth: {
-              user: emailAddress,
-              pass: password
-            },
-            json: true,
-            body: requestBody
-          }).then(function (response) {
-            assert(userBiz.read.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
-            assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
-          });
-        });
-
-        it('should update on PUT', function () {
-          var requestMatcher = sinon.match({
-            auth: sinon.match(requestAuth),
-            params: sinon.match({}),
-            query: sinon.match({}),
-            body: sinon.match(requestBody)
-          });
-          return ajax({
-            method: 'PUT',
-            uri: 'http://localhost:3000/api/users/123',
-            auth: {
-              user: emailAddress,
-              pass: password
-            },
-            json: true,
-            body: requestBody
-          }).then(function (response) {
-            assert(userBiz.update.withArgs(requestMatcher).calledOnce, 'Create was not called properly.');
-            assert.deepEqual(response.body, responseBody, 'The router returned the wrong thing.');
-          });
-        });
-
       });
 
     });
