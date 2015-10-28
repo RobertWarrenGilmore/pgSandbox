48c88a3ba271f9767d80e80c34c67ff42a93ced1
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-10-28 16:11:26 -0700

    Removed comments.

--- client/main.jsx
+++ client/main.jsx
@@ -14,7 +14,7 @@ var NotFound = require('./views/notFound.jsx');
 function requireAuth(nextState, replaceState) {
   if (!flux.store('auth').getAuth()) {
     replaceState({
-      nextPathname: nextState.location.pathname //TODO This has something to do with this: https://github.com/rackt/react-router/blob/master/examples/auth-with-shared-root/components/Login.js#L15
+      nextPathname: nextState.location.pathname
     }, '/login');
   }
 }
@@ -39,7 +39,6 @@ document
     }
     var router = (
       <Router createElement={addFlux} history={createBrowserHistory()}>
-        {/* TODO Does this flux declaration work with the mixin? */}
         <Route component={App} path='/'>
 
           <Route onEnter={denyAuth}>
