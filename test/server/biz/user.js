3b73d93df786cbc7b4f1869158f6e9c3037835f1
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-12-17 05:41:22 -0500

    Moved knex destruction up to the server suite.

--- test/server/biz/user.js
+++ test/server/biz/user.js
@@ -40,10 +40,6 @@ describe('user', function () {
     });
   });
 
-  after('Destroy knex.', function () {
-    return knex.destroy();
-  });
-
   describe('create', function () {
     var emailAddress = 'mocha.test.email.address@not.a.real.domain.com';
     var badEmailAddress = 'NotAValidEmailAddress.com';
--- test/server/server.js
+++ test/server/server.js
@@ -1,5 +1,11 @@
+var knex = require('../../server/database/knex');
+
 describe('server', function () {
   // a list of all of the server-side test modules to run
   require('./biz/biz');
   require('./transport/transport');
+
+  after('Destroy knex.', function () {
+    return knex.destroy();
+  });
 });
