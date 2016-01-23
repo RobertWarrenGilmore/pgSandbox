9bba07dd29fe02ad0f5dd569267fd21981eb2541
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2016-01-23 01:08:23 -0800

    Fixed a problem with openPort.

--- openPort.sh
+++ openPort.sh
@@ -1,5 +1,6 @@
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8000
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-port 44300
-iptables-save > /etc/iptables/rules.v4
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT;
+iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8000;
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT;
+iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-port 44300;
+mkdir /etc/iptables;
+iptables-save > /etc/iptables/rules.v4;
