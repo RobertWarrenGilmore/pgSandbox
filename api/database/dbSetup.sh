bbdc7fcbb5276f9265a81aa4e4bcb69ce3f0c8d1
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2016-05-08 20:34:38 -0700

    generic dbSetup

--- api/database/dbSetup.sh
+++ api/database/dbSetup.sh
@@ -3,7 +3,7 @@ echo "Enter a password for the database user:"
 read dbPassword
 
 sudo apt-get --assume-yes install postgresql postgresql-contrib
-sudo -u postgres psql --command="CREATE ROLE \"adjuvet\" LOGIN PASSWORD '${dbPassword}' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
-sudo -u postgres psql --command="CREATE DATABASE \"adjuvet\" WITH ENCODING='UTF8' OWNER=\"adjuvet\" CONNECTION LIMIT=-1;"
-sudo -u postgres psql --command="CREATE DATABASE \"adjuvetTest\" WITH ENCODING='UTF8' OWNER=\"adjuvet\" CONNECTION LIMIT=-1;"
+sudo -u postgres psql --command="CREATE ROLE \"sandboxUser\" LOGIN PASSWORD '${dbPassword}' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
+sudo -u postgres psql --command="CREATE DATABASE \"sandbox\" WITH ENCODING='UTF8' OWNER=\"sandboxUser\" CONNECTION LIMIT=-1;"
+sudo -u postgres psql --command="CREATE DATABASE \"sandboxTest\" WITH ENCODING='UTF8' OWNER=\"sandboxUser\" CONNECTION LIMIT=-1;"
 echo "dbPassword=${dbPassword}" >> .env
