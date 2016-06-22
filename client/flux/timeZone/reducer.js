258b58d0646d2cf9d7291013fdfb910868e24ae6
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2016-06-21 17:00:48 -0700

    Fixed a bug in the time zone reducer that prevented log-in.

--- client/flux/timeZone/reducer.js
+++ client/flux/timeZone/reducer.js
@@ -8,7 +8,7 @@ const initialState = moment.tz.guess()
 const reducer = handleActions({
   [types.SET_TIME_ZONE]: (state = initialState, action) => {
     const timeZoneName = action.payload
-    const timeZone = (moment.tz.zone(timeZoneName) || moment.tz.guess()).name
+    const timeZone = (moment.tz.zone(timeZoneName || moment.tz.guess())).name
     return timeZone
   }
 }, initialState)
