9d72bc20a9706ed1acea1a6b3086855221438aba
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2016-06-21 17:16:33 -0700

    Miscellaneous refactor of log-in route handler.

--- client/views/logInRouteHandler.jsx
+++ client/views/logInRouteHandler.jsx
@@ -2,28 +2,41 @@
 const React = require('react')
 const { Link } = require('react-router')
 const { connect } = require('react-redux')
-const { logIn } = require('../flux/auth/actions')
+const { logIn: logInAction } = require('../flux/auth/actions')
 const Helmet = require('react-helmet')
+const ErrorMessage = require('./errorMessage.jsx')
 
 class LogIn extends React.Component {
+  static propTypes = {
+    busy: React.PropTypes.bool,
+    logIn: React.PropTypes.func
+  };
+  static defaultProps = {
+    busy: false,
+    logIn: null
+  };
+  state = {
+    error: null,
+    emailAddress: '',
+    password: ''
+  };
   constructor(props) {
     super(props)
-    this.state = {
-      error: null
-    }
+    this._onChangeEmailAddress = this._onChangeEmailAddress.bind(this)
+    this._onChangePassword = this._onChangePassword.bind(this)
     this._onSubmit = this._onSubmit.bind(this)
   }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.credentials) {
-      const location = this.props.location
-      if (location.state && location.state.nextLocation) {
-        this.props.history.replaceState(null, location.state.nextLocation.pathname, location.state.nextLocation.query)
-      } else {
-        this.props.history.replaceState(null, '/')
-      }
-    }
-  }
   render() {
+    const {
+      props: {
+        busy
+      },
+      state: {
+        error,
+        emailAddress,
+        password
+      }
+    } = this
     const location = this.props.location
     const nextLocation = location.state ? location.state.nextLocation : null
     return (
@@ -38,55 +51,88 @@ class LogIn extends React.Component {
           </p>
         ) : null}
         <form onSubmit={this._onSubmit}>
-          <input type='email' ref='emailAddress' name='emailAddress' placeholder='email address' disabled={this.state.busy} required/>
-          <input type='password' ref='password' name='password' placeholder='password' disabled={this.state.busy} required/>
-          {this.state.error
-            ? <p className='error'>
-                {this.state.error}
-              </p>
-            : null}
+          <input
+            type='email'
+            value={emailAddress}
+            onChange={this._onChangeEmailAddress}
+            placeholder='email address'
+            disabled={busy}
+            required
+            />
+          <input
+            type='password'
+            value={password}
+            onChange={this._onChangePassword}
+            placeholder='password'
+            disabled={busy}
+            required
+            />
+          {error ?
+            <ErrorMessage error={error}/>
+          : null}
           <div className='actions'>
             <Link to='/forgotPassword'>
               Are you locked out?
             </Link>
-            <button disabled={this.state.busy} className='highlighted'>log in</button>
+            <button
+              disabled={busy}
+              className='highlighted'
+              >
+              log in
+            </button>
           </div>
         </form>
       </div>
     )
   }
+  _onChangeEmailAddress({target: {value}}) {
+    this.setState({
+      emailAddress: value
+    })
+  }
+  _onChangePassword({target: {value}}) {
+    this.setState({
+      password: value
+    })
+  }
   _onSubmit(event) {
     event.preventDefault()
-    this.props.logIn({
-      emailAddress: this.refs.emailAddress.value,
-      password: this.refs.password.value
+    const {
+      props: {
+        logIn
+      },
+      state: {
+        emailAddress,
+        password
+      }
+    } = this
+    return logIn({
+      emailAddress,
+      password
+    })
+    .then(() => {
+      const location = this.props.location
+      if (location.state && location.state.nextLocation) {
+        this.props.history.replaceState(null, location.state.nextLocation.pathname, location.state.nextLocation.query)
+      } else {
+        this.props.history.replaceState(null, '/')
+      }
     })
     .catch(err => this.setState({
       error: err.message || err
     }))
   }
 }
-LogIn.propTypes = {
-  credentials: React.PropTypes.object,
-  busy: React.PropTypes.bool,
-  logIn: React.PropTypes.func
-}
-LogIn.defaultProps = {
-  credentials: null,
-  busy: false,
-  logIn: null
-}
 
 const wrapped = connect(
   function mapStateToProps(state) {
     return {
-      credentials: state.auth.credentials,
       busy: state.auth.busy
     }
   },
   function mapDispatchToProps(dispatch) {
     return {
-      logIn: (credentials) => dispatch(logIn(credentials))
+      logIn: (credentials) => dispatch(logInAction(credentials))
     }
   }
 )(LogIn)
